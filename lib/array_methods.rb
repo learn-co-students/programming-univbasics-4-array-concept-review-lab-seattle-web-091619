567d3bb79624c9399a8ddc5ba64ab3bd31712ade
Katrina R <katrina.reinsdorff@gmail.com>
2019-08-30 14:49:10 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,11 +5,11 @@ end
 def find_max_value(array) 
     new_array = []
     new_array = array.sort!
-    return new_array[-1]
+    new_array[-1]
 end
 
 def find_min_value(array)
     new_array = []
     new_array = array.sort!
-    return new_array[0]
+    new_array[0]
 end
