0bfa7a88a6ed0e87414b04d18fa7d059ca8c8556
Andrew Read <andyreadpnw@gmail.com>
2019-09-16 03:09:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+   array.length.times do |index|
+    if array[index] == value_to_find
+      return array[index]
 end
 
 def find_max_value(array)
-  # Add your solution here
+  find_max_value([1,2,1,3,4,3,5,4,3,2,1])).to eq(5)
 end
 
 def find_min_value(array)
-  # Add your solution here
+   find_min_value([11,6,4,8,20,2,4,10])).to eq(2)
 end
